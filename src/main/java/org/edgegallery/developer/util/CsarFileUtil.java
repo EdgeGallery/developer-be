5c8065a22c9329a75d8f37108a903b7cb8773786
liuhuiling <liuhuiling4@huawei.com>
2020-12-01 10:09:09 +0800

    add atp interface code
    add atp interface code
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/developer/util/CsarFileUtil.java
+++ src/main/java/org/edgegallery/developer/util/CsarFileUtil.java
@@ -1,15 +1,17 @@
 /*
- * Copyright 2020 Huawei Technologies Co., Ltd.
+ *    Copyright 2020 Huawei Technologies Co., Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package org.edgegallery.developer.util;
@@ -29,7 +31,8 @@ public final class CsarFileUtil {
 
     private static final int BUFFER_SIZE = 2 * 1024 * 1024;
 
-    private CsarFileUtil() {}
+    private CsarFileUtil() {
+    }
 
     /**
      * unzip zip file.
@@ -53,7 +56,7 @@ public final class CsarFileUtil {
                 }
 
                 try (InputStream input = zipFile.getInputStream(entry);
-                        BufferedOutputStream bos = new BufferedOutputStream(FileUtils.openOutputStream(file));) {
+                     BufferedOutputStream bos = new BufferedOutputStream(FileUtils.openOutputStream(file));) {
                     int length = 0;
                     if (input != null) {
                         while ((length = input.read(buffer)) != -1) {
@@ -77,4 +80,5 @@ public final class CsarFileUtil {
         }
         return true;
     }
+
 }
