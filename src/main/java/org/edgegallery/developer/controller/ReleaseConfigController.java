f457eb501b9e3013e50ad12e0942fc4730883df9
helongfei <sinosoft30170@sina.com>
2020-12-02 09:18:26 +0800

    添加release-config的post,put,get接口

---
+++ src/main/java/org/edgegallery/developer/controller/ReleaseConfigController.java
@@ -0,0 +1,81 @@
+package org.edgegallery.developer.controller;
+
+import com.spencerwi.either.Either;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import javax.validation.constraints.Pattern;
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.edgegallery.developer.model.ReleaseConfig;
+import org.edgegallery.developer.response.ErrorRespDto;
+import org.edgegallery.developer.response.FormatRespDto;
+import org.edgegallery.developer.service.ReleaseConfigService;
+import org.edgegallery.developer.util.ResponseDataUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RestSchema(schemaId = "releaseconfig")
+@RequestMapping("/mec/developer/v1/releaseconfig")
+public class ReleaseConfigController {
+    private static final String REGEX_UUID = "[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}";
+
+    @Autowired
+    private ReleaseConfigService releaseService;
+
+    @ApiOperation(value = "save release config", response = ReleaseConfig.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ReleaseConfig.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    @RequestMapping(value = "/{projectId}/action/save-release-config", method = RequestMethod.POST,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PreAuthorize("hasRole('DEVELOPER_TENANT')")
+    public ResponseEntity<ReleaseConfig> saveReleaseConfig(
+        @Pattern(regexp = REGEX_UUID, message = "projectId must be in UUID format")
+        @ApiParam(value = "projectId", required = true) @PathVariable(value = "projectId", required = true)
+            String projectId, @RequestBody ReleaseConfig config) {
+        Either<FormatRespDto, ReleaseConfig> either = releaseService.saveConfig(projectId, config);
+        return ResponseDataUtil.buildResponse(either);
+    }
+
+    @ApiOperation(value = "modify release config", response = ReleaseConfig.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ReleaseConfig.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    @RequestMapping(value = "/{projectId}/action/modify-release-config", method = RequestMethod.PUT,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PreAuthorize("hasRole('DEVELOPER_TENANT')")
+    public ResponseEntity<ReleaseConfig> updateReleaseConfig(
+        @Pattern(regexp = REGEX_UUID, message = "projectId must be in UUID format")
+        @ApiParam(value = "projectId", required = true) @PathVariable(value = "projectId", required = true)
+            String projectId, @RequestBody ReleaseConfig config) {
+        Either<FormatRespDto, ReleaseConfig> either = releaseService.modifyConfig(projectId, config);
+        return ResponseDataUtil.buildResponse(either);
+    }
+
+    @ApiOperation(value = "get release config", response = ReleaseConfig.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK", response = ReleaseConfig.class),
+        @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
+    })
+    @RequestMapping(value = "/{projectId}/action/get-release-config", method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PreAuthorize("hasRole('DEVELOPER_TENANT')")
+    public ResponseEntity<ReleaseConfig> getReleaseConfig(
+        @Pattern(regexp = REGEX_UUID, message = "projectId must be in UUID format")
+        @ApiParam(value = "projectId", required = true) @PathVariable(value = "projectId", required = true)
+            String projectId) {
+        Either<FormatRespDto, ReleaseConfig> either = releaseService.getConfigById(projectId);
+        return ResponseDataUtil.buildResponse(either);
+    }
+}
---
+++ src/main/java/org/edgegallery/developer/mapper/ReleaseConfigMapper.java
@@ -0,0 +1,18 @@
+package org.edgegallery.developer.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.edgegallery.developer.model.ReleaseConfig;
+
+@Mapper
+public interface ReleaseConfigMapper {
+
+    int saveConfig(ReleaseConfig config);
+
+    ReleaseConfig getConfigByReleaseId(String releaseId);
+
+    ReleaseConfig getConfigByProjectId(@Param("projectId") String projectId);
+
+    int modifyReleaseConfig(ReleaseConfig config);
+
+}
