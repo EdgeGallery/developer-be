4f283756db7855f0a37600a6dfecb0bca3526373
zhanghailong22 <zhanghailong22@huawei.com>
2021-03-01 10:20:21 +0800

    适配atp接口

--- src/main/java/org/edgegallery/developer/util/AtpUtil.java
+++ src/main/java/org/edgegallery/developer/util/AtpUtil.java
@@ -62,7 +62,6 @@ public class AtpUtil {
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         LOGGER.info("filePath: {}", filePath);
         body.add("file", new FileSystemResource(filePath));
-        body.add("isRun", Boolean.TRUE);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
