7c9653a4df3fd1af52fd5e1f24b1cf36cb563466
zhanghailong22 <zhanghailong22@huawei.com>
2021-03-11 19:43:30 +0800

    修改 lcm接口

---
+++ src/main/java/org/edgegallery/developer/model/LcmLog.java
@@ -0,0 +1,14 @@
+package org.edgegallery.developer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LcmLog {
+
+    private String log;
+
+}
