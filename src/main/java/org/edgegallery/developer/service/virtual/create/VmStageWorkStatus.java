9c52334b45b16aeb9da94ad91afb8fc93b7c0031
zhanghailong22 <zhanghailong22@huawei.com>
2021-03-18 21:06:52 +0800

    修改虚机部署信息

--- src/main/java/org/edgegallery/developer/service/virtual/create/VmStageWorkStatus.java
+++ src/main/java/org/edgegallery/developer/service/virtual/create/VmStageWorkStatus.java
@@ -1,13 +1,19 @@
 package org.edgegallery.developer.service.virtual.create;
 
+import java.lang.reflect.Type;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import org.edgegallery.developer.mapper.ProjectMapper;
+import org.edgegallery.developer.model.deployyaml.PodStatusInfos;
 import org.edgegallery.developer.model.vm.VmCreateConfig;
+import org.edgegallery.developer.model.vm.VmInstantiateInfo;
 import org.edgegallery.developer.model.workspace.ApplicationProject;
 import org.edgegallery.developer.model.workspace.EnumTestConfigStatus;
+import org.edgegallery.developer.model.workspace.MepHost;
 import org.edgegallery.developer.service.virtual.VmService;
+import org.edgegallery.developer.util.HttpClientUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,13 +41,12 @@ public class VmStageWorkStatus implements VmCreateStage {
     public boolean execute(VmCreateConfig config) throws InterruptedException {
         boolean processStatus = false;
         EnumTestConfigStatus status = EnumTestConfigStatus.Failed;
+        Type type = new TypeToken<MepHost>() { }.getType();
+        MepHost host = gson.fromJson(gson.toJson(config.getHost()), type);
 
         ApplicationProject project = projectMapper.getProjectById(config.getProjectId());
-        String workStatus =
-            "{\"pods\":[{\"podstatus\":\"Running\",\"podname\":\"positioning\",\"containers\":[{\"containername\":"
-                + "\"positioning\",\"metricsusage\":{\"cpuusage\":\"90/4000\",\"memusage\":\"81469440/16714080256\","
-                + "\"diskusage\":\"0/94877588119\"}},{\"containername\":\"mep-agent\",\"metricsusage\":{\"cpuusage\":"
-                + "\"0/4000\",\"memusage\":\"8871936/16714080256\",\"diskusage\":\"0/94877588119\"}}]}]}";
+        String workStatus =HttpClientUtil.getWorkloadStatus(host.getProtocol(), host.getLcmIp(), host.getPort(),
+            config.getAppInstanceId(), project.getUserId(), config.getLcmToken());
         if (workStatus == null) {
             // compare time between now and deployDate
             long time = System.currentTimeMillis() - config.getCreateTime().getTime();
@@ -58,8 +63,11 @@ public class VmStageWorkStatus implements VmCreateStage {
             processStatus = true;
             status = EnumTestConfigStatus.Success;
             config.setLog("get vm status success");
-            JsonObject jsonObject = new JsonParser().parse(workStatus).getAsJsonObject();
-            LOGGER.info("Query create vm info response: {}, json {}", workStatus, jsonObject);
+            Type vmInfoType = new TypeToken<VmInstantiateInfo>() { }.getType();
+            VmInstantiateInfo vmInstantiateInfo = gson.fromJson(workStatus, vmInfoType);
+            config.setVmInfo(vmInstantiateInfo.getData());
+
+            LOGGER.info("Query create vm info response: {}", workStatus);
         }
         // update test-config
         vmService.updateCreateVmResult(config, project, "workStatus", status);
