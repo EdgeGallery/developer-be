c2d9cfa39093b9426417580814b30f20825f0b4f
helongfei <sinosoft30170@sina.com>
2021-07-29 09:13:19 +0800

    增加容器镜像的增删改查接口

---
+++ src/main/java/org/edgegallery/developer/model/containerimage/EnumContainerImageStatus.java
@@ -0,0 +1,10 @@
+package org.edgegallery.developer.model.containerimage;
+
+public enum EnumContainerImageStatus {
+    UPLOAD_WAIT,
+    UPLOADING,
+    UPLOADING_MERGING,
+    UPLOAD_SUCCEED,
+    UPLOAD_FAILED,
+    UPLOAD_CANCELLED
+}
