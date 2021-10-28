8fee672390cc7a6a0ddf07e8de63fc09e59d7a63
zhanghailong22 <zhanghailong22@huawei.com>
2021-10-28 10:44:40 +0800

    新增aptTestTask模型

---
+++ src/main/java/org/edgegallery/developer/model/atpTestTask/AtpTestTask.java
@@ -0,0 +1,18 @@
+package org.edgegallery.developer.model.atpTestTask;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AtpTestTask {
+
+    String id;
+
+    String appName;
+
+    String status;
+
+    String createTime;
+
+}
