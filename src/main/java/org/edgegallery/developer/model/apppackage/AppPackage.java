5495d8b0b794f2aac32a22b0ea1a7bcb3a9ed86d
twilight0620 <liuhuiling4@huawei.com>
2021-10-29 16:49:52 +0800

    change method name
    change method name
    
    Issue-ID: I1TDH6
    
    Signed-off-by: liuhuiling <liuhuiling4@huawei.com>

--- src/main/java/org/edgegallery/developer/model/apppackage/AppPackage.java
+++ src/main/java/org/edgegallery/developer/model/apppackage/AppPackage.java
@@ -39,7 +39,7 @@ public class AppPackage {
      *
      * @return package path
      */
-    public String getPkgPath() {
+    public String computePkgPath() {
         return InitConfigUtil.getWorkSpaceBaseDir().concat(BusinessConfigUtil.getWorkspacePath()).concat(appId)
             .concat(File.separator).concat(packageFileName);
     }
