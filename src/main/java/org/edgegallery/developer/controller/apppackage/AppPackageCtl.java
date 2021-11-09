810006273464de483e3c69ad6c1c5824b6660457
helongfei <sinosoft30170@sina.com>
2021-11-09 16:47:16 +0800

    update

--- src/main/java/org/edgegallery/developer/controller/apppackage/AppPackageCtl.java
+++ src/main/java/org/edgegallery/developer/controller/apppackage/AppPackageCtl.java
@@ -111,7 +111,7 @@ public class AppPackageCtl {
         @ApiResponse(code = 200, message = "OK", response = String.class),
         @ApiResponse(code = 400, message = "Bad Request", response = ErrorRespDto.class)
     })
-    @RequestMapping(value = "/{packageId}/file-content", method = RequestMethod.PUT,
+    @RequestMapping(value = "/{packageId}/action/update-file-content", method = RequestMethod.PUT,
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @PreAuthorize("hasRole('DEVELOPER_TENANT') || hasRole('DEVELOPER_ADMIN')")
     public ResponseEntity<Boolean> updateAppPackageFileContent(
