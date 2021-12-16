0e421ea25f7c975891fc2ef493a88401556ab61f
zhanghailong22 <zhanghailong22@huawei.com>
2021-12-16 21:19:49 +0800

    删除老的webssh，统一用common-webssh

--- src/main/java/org/edgegallery/developer/filter/security/AccessTokenFilter.java
+++ src/main/java/org/edgegallery/developer/filter/security/AccessTokenFilter.java
@@ -51,8 +51,7 @@ public class AccessTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-        if (request.getRequestURI() == null || !(request.getRequestURI().equals("/health") || request.getRequestURI()
-            .equals("/webssh"))) {
+        if (request.getRequestURI() == null || !(request.getRequestURI().equals("/health"))) {
             String accessTokenStr = request.getHeader(Consts.ACCESS_TOKEN_STR);
             if (StringUtils.isEmpty(accessTokenStr)) {
                 LOGGER.error("Access token is empty");
